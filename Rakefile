1959815dd232cd25d9050e12dd3595bb5478ffbc
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro-2.local>
2016-11-08 15:23:18 -0600

    feeling great about ruby, oh ya

---
+++ Rakefile
@@ -0,0 +1,8 @@
+# like gulp
+require 'sinatra/activerecord'
+require 'sinatra/activerecord/rake'
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'mysql2',
+	:database => 'superstars'	
+)
--- controllers/superstar_controller.rb
+++ controllers/superstar_controller.rb
@@ -1,27 +1,73 @@
 class SuperstarController < ApplicationController
-	get '/' do
-		{:message => 'all items soon'}.to_json
+	get '/' do                       # in browser can go to localhost: 9292/api/superstar 
+		#get all
+		Superstar.all.to_json
 	end 	
 
-	get '/fab' do
-		# teach you how to rule the database
-		# use activerecord 
-		@christopher = Superstar.new
-		@christopher.talent = 'Socialite'
-		@christopher.name = 'Christopher McAwesome'
-		@christopher.outfit = 'blue sweater'
-		@christopher.save
-		@christopher.to_json
 
-		
-		# crud
+	# RESTful controller (REST API with RESTful routes)
+
+	get '/:id' do
+		# get by id
+		@id = params[:id]            # in browser u can see by ids http://localhost:9292/api/superstars/2
+		Superstar.find(@id).to_json
+	end 	
+
+	post '/' do
 		# create
+		@name = params[:name]
+		@talent = params[:talent]
+		@outfit = params[:outfit]
+
+		@model = Superstar.new
+		@model.name = @name
+		@model.talent = @talent
+		@model.outfit = @outfit
+		@model.save
+
+		# place a breakpoint in ruby!
+		# binding.pry
+		# ^ the code stops here
+		@model.to_json
+	end
+
+	patch '/:id' do
 		# update
-		# read
-		# destroy
+		@id = params[:id]
+		@name = params[:name]
+		@talent = params[:talent]
+		@outfit = params[:outfit]	
+		@model = Superstar.find(@id)	
+		@model.name = @name
+		@model.talent = @talent
+		@model.outfit = @outfit
+		@model.save.to_json
+	end
+
+	delete '/:id' do
+		# delete
+		@id = params[:id]
+		@model = Superstar.find(@id)
+		@model.destroy
+		{ :message => 'Item of id ' + @id + ' was removed.' }.to_json
+	end  	
 
 
-
-	end	
+	# get '/fab' do
+	# 	# teach you how to rule the database
+	# 	# use activerecord 
+	# 	@christopher = Superstar.new
+	# 	@christopher.talent = 'Socialite'
+	# 	@christopher.name = 'Christopher McAwesome'
+	# 	@christopher.outfit = 'blue sweater'
+	# 	@christopher.save
+	# 	@christopher.to_json
+		
+	# 	# crud
+	# 	# create
+	# 	# update
+	# 	# read
+	# 	# destroy
+	# end	
 
 end 
